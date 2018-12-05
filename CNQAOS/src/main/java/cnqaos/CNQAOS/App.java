c7ab1a918c34e4eeaac7960912e301955ed86ce4
nitintajane3 <nitintajane3@gmail.com>
2018-12-05 16:30:36 +0530

    first commit

---
+++ CNQAOS/src/main/java/cnqaos/CNQAOS/App.java
@@ -0,0 +1,13 @@
+package cnqaos.CNQAOS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
